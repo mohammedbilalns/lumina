7f3e9403a7d06bd0022d01dce6633b23272e4e7f
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-05-05 13:55:40 +0530

    configured modules

---
+++ plan/backend.md
@@ -0,0 +1,9 @@
+# Modules 
+- Auth - signup login logout  
+- Users - get profile , update profile , change password   
+- Categories - list categories , seed categories , 
+- Preferences - add preferences , remove preferences , get preferences 
+- Articles - Create , edit , delete , view , list own articles 
+- Feed - 
+- Reactions module - like , dislike , block 
+- files ( if there is image )
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { PreferencesModule } from './preferences/preferences.module';
+import { ArticlesModule } from './articles/articles.module';
+import { ReactionsModule } from './reactions/reactions.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CategoriesModule, PreferencesModule, ArticlesModule, ReactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
