1dc1514dcfc60bbab686fdeef3815e6eb45ea215
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-05-06 11:54:28 +0530

    feat : configured dtos

---
+++ server/src/users/dtos/change-password.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+
+export class ChangePasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  oldPassword: string
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+    message:
+      'Password must contain uppercase, lowercase, number, and special character',
+  })
+  newPassword: string
+
+}
