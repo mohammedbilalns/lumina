e800b84d3338396ce58a8923d5313d60216e2381
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-05-09 15:40:29 +0530

    feat : landing page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,14 +1,5 @@
+import { Landing } from '#/views/landing'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute('/')({ component: Landing })
 
-function Home() {
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
-  )
-}
