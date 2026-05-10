4a0aa88d21978b3525700c5db93035993cc2ce57
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-05-10 14:45:55 +0530

    chore : send token as data , request logger

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -3,15 +3,13 @@ import {
   Controller,
   Post,
   Req,
-  Res,
   UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
-import type { Response, Request } from 'express';
-import { ConfigService } from '@nestjs/config';
+import type { Request } from 'express';
 import { JwtGuard } from '../security/guards/jwt/jwt.guard';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { type JwtPayload } from './types/jwt-payload.type';
@@ -25,7 +23,6 @@ import { ResetPasswordDto } from './dto/reset-password.dto';
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-    private readonly configService: ConfigService,
   ) {}
 
   @Post('signup')
@@ -41,20 +38,14 @@ export class AuthController {
   @Post('signup/verify-otp')
   async verifySignupOtp(
     @Body() data: VerifySignupOtpDto,
-
-    @Res({
-      passthrough: true,
-    })
-    response: Response,
   ) {
     const result = await this.authService.verifySignupOtp(data);
-
-    this.setRefreshCookie(result.refreshToken, response);
 
     return {
       message: 'Signup successful',
       data: {
         accessToken: result.accessToken,
+        refreshToken: result.refreshToken,
         user: result.user,
       },
     };
@@ -102,19 +93,14 @@ export class AuthController {
   @Post('login')
   async login(
     @Body() data: LoginDto,
-
-    @Res({
-      passthrough: true,
-    })
-    response: Response,
   ) {
     const result = await this.authService.login(data);
-    this.setRefreshCookie(result.refreshToken, response);
 
     return {
       message: 'Login successful',
       data: {
         accessToken: result.accessToken,
+        refreshToken: result.refreshToken,
         user: result.user,
       },
     };
@@ -125,17 +111,10 @@ export class AuthController {
   async logout(
     @CurrentUser()
     user: JwtPayload,
-
-    @Res({
-      passthrough: true,
-    })
-    response: Response,
   ) {
     await this.authService.logout({
       userId: user.sub,
     });
-
-    response.clearCookie('refreshToken');
 
     return { message: 'Logged out successfully' };
   }
@@ -144,13 +123,10 @@ export class AuthController {
   async refreshToken(
     @Req()
     request: Request,
-
-    @Res({
-      passthrough: true,
-    })
-    response: Response,
+    @Body('refreshToken') 
+    bodyRefreshToken?: string,
   ) {
-    const refreshToken = request.cookies['refreshToken'] as string | undefined;
+    const refreshToken = bodyRefreshToken || (request.cookies['refreshToken'] as string | undefined);
 
     if (!refreshToken) {
       throw new UnauthorizedException('Invalid token');
@@ -160,22 +136,13 @@ export class AuthController {
       refreshToken,
     });
 
-    this.setRefreshCookie(result.refreshToken, response);
     return {
       message: 'Refresh token successful',
       data: {
         accessToken: result.accessToken,
+        refreshToken: result.refreshToken,
         user: result.user,
       },
     };
-  }
-
-  private setRefreshCookie(refreshToken: string, res: Response) {
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      secure: this.configService.get('NODE_ENV') === 'production',
-      sameSite: 'lax',
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
 
   app.useGlobalPipes(
     new ValidationPipe({
