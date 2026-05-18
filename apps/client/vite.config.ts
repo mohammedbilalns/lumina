0883d7041fd4b051b6a1cccabfadeae722f0c26f
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-05-18 17:41:31 +0530

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,15 +1,17 @@
-
 import { defineConfig } from 'vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [  viteReact(), tailwindcss(), tanstackStart(), netlify()],
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tanstackStart(),
+    viteReact(),
+    tailwindcss(),
+    netlify(),
+  ],
 })
-
-export default config
